d0e6c95eb58a578efe7108fc4f9f770bcc905c61
Derrick Low <lowderrickym@gmail.com>
2025-08-14 19:18:56 +0800

    feat(implementing-hash-tables): added testing and logging statemenets for implementing of hash tables

---
+++ TestHash.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class TestHash {
+
+	public static void main(String[] args) {
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		// placing items into the hashmap
+		hm.put("Mike", 52);
+		hm.put("Janet", 46);
+		hm.put("Jack", 46);
+		// retrieving item from the hashmap
+		System.out.println("Janet => " + hm.get("Janet"));
+
+		// remove item from the hashmap
+		hm.remove("Mike");
+		System.out.println("Mike => " + hm.get("Mike"));
+		
+	}
+}
