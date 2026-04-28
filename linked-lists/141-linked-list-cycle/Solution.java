f401004bc683471f2c32d67453d8c940c5174199
Derrick Low <lowderrickym@gmail.com>
2026-04-28 08:14:33 +0800

    add solution to linked list cycle

---
+++ linked-lists/141-linked-list-cycle/Solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public boolean hasCycle(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
